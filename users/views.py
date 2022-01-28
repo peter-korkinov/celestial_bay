690aa017ab54e832dde7918ab78d99696ef7f8f6
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-01-28 21:52:12 +0200

    starting to build user authentication functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import MyUserSerializer
+
+
+class RegisterUser(APIView):
+    def post(self, request):
+        serializer = MyUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
