25250a97e31864281c91eadf37c57e84086d4804
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-02-13 17:34:45 +0200

    overriding MyUser's create function so that password stored in db to be hashed

--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,6 @@ class MyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = '__all__'
+
+    def create(self, validated_data):
+        return MyUser.objects.create_user(**validated_data)
